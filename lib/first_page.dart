9e912608cbe50d44e7291dd65e5cfbdb3060c99f
anas altarazi <anas.anas1998.tar@gmail.com>
2022-10-22 06:51:36 -0700

    drawer example

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -35,6 +35,60 @@ class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            UserAccountsDrawerHeader(
+              accountName: Text('account name'),
+              accountEmail: Text('email'),
+              currentAccountPicture: CircleAvatar(
+                child: FlutterLogo(size: 70),
+              ),
+              currentAccountPictureSize: Size(75, 75),
+              decoration: BoxDecoration(
+                  image:
+                      DecorationImage(image: AssetImage('assets/nature.jpeg'))),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: const Text('BottomNavigationBar Sample'),
       ),
